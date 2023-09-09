6297092e451d26d4d6c46df24ee1d822861ed457
Enoque Rogerio <enoquerogerio15mti25@gmail.com>
2023-09-09 19:28:02 +0100

    method that makes a call to an API and returns a specific property

--- productService.js
+++ productService.js
@@ -1,7 +1,12 @@
 async function getProduct(productId) {
-    const apiUrl = `https://fakestoreapi.com/products/${productId}`;
+  const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
-    // Add your solution here!
+  // Add your solution here!
+  const product = await fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => error);
+  return product;
 }
 
 module.exports = getProduct;
